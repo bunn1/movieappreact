40970cbd4991d1281cf5a8d9c1f8dd3a91e6a541
bunn1 <nicjohbun@gmail.com>
2024-04-22 15:12:57 +0200

    function handleOnSubmit fetch API movies.map container Styling header, search display movie poster with ternary operator

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,12 +3,32 @@ import React from 'react';
 const IMG_API = 'https://image.tmdb.org/t/p/w1280';
 
 function Movie({ title, poster_path, overview, vote_average }) {
+  const setVoteClass = () => {
+    if (vote_average >= 8) {
+      return 'green';
+    } else if (vote_average >= 6) {
+      return 'orange';
+    } else {
+      return 'red';
+    }
+  };
+
   return (
     <div className="movie">
-      <img src={IMG_API + poster_path} alt={title} />
+      <img
+        src={
+          poster_path
+            ? IMG_API + poster_path
+            : 'https://images.unsplash.com/photo-1542204165-65bf26472b9b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bW92aWV8ZW58MHx8MHx8fDA%3D'
+        }
+        alt={title}
+      />
       <div className="movie-info">
         <h3>{title}</h3>
-        <span>{vote_average}</span>
+        {/* Conditionally set the className for the span tag */}
+        <span className={`tag ${setVoteClass(vote_average)}`}>
+          {vote_average}
+        </span>
       </div>
 
       <div className="movie-overview">
